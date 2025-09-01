6b944099054ce15a08d52c33597237eff14ec401
dipok-1 <dipokdutta8099@gmail.com>
2025-09-01 23:08:26 +0600

    updated the cors origin

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -8,7 +8,7 @@ mongodb()
 const octokit = new Octokit({auth:process.env.auth});
 import cors from 'cors';
 app.use(cors({
-  origin: 'http://localhost:5173'  // Allow frontend origin
+  origin: 'https://git-profile-viewer.onrender.com'  // Allow frontend origin
 }));
 app.use(express.json())
 
